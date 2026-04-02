6048b4b1bc3f5e98019861cb61d4c042d62eda84
Philipp Hossner <phil@deepl.com>
2026-04-02 04:53:17 +0200

    Make GHC RTS shim Linux-only
    The __fini_array_start/end symbols are ELF-specific and only needed on
    Linux. macOS uses Mach-O which does not have these symbols.

--- ext_lib/ghc_shim.c
+++ ext_lib/ghc_shim.c
@@ -2,11 +2,16 @@
  * Shim to provide linker symbols expected by GHC RTS when linking
  * as a shared library. These symbols are normally provided by the
  * linker only for executables.
+ *
+ * Only needed on Linux (ELF). macOS (Mach-O) does not use these symbols.
  */
 
-/* Dummy fini/init array bounds for GHC RTS */
+#ifdef __linux__
+
 __attribute__((visibility("hidden")))
 void (*__fini_array_start[0])() __attribute__((section(".fini_array"))) = {};
 
 __attribute__((visibility("hidden")))
 void (*__fini_array_end[0])() = {};
+
+#endif
